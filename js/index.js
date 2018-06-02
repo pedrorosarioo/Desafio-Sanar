d7fd242bd49930e822f993a6a81bdde70a48a133
Pedro Rosario <jrosario.pedro@gmail.com>
2018-06-02 14:37:27 -0300

    Adcionando Scroll com javascript

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function showDescription(){
+	var description = document.getElementById("desc1");
+	description.scrollIntoView();
+};
+
+function changeColor(){
+	alert('dose');
+};
+
+alert("jsCarregado");
---
+++ js/indexEvents.js
@@ -0,0 +1,12 @@
+"use strict";
+
+alert('eoq');
+
+function showDescription(){
+	var description = document.getElementById("desc1");
+	description.scrollIntoView();
+}
+
+function changeColor(){
+	alert('dose');
+}
