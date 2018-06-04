9999d81fac7662c2a69dd4a75dc3c738ec7ef22e
Pedro Rosario <jrosario.pedro@gmail.com>
2018-06-03 23:28:25 -0300

    Adcionando função de troca de cores dos botões Sessão Selly

--- js/index.js
+++ js/index.js
@@ -16,6 +16,28 @@ function showDescription(event){
 };
 
 function changeColor(event){
-				
+	bt1=document.getElementById("bt1");
+	bt2=document.getElementById("bt2");
+	switch(event.target.id){
+		case ("bt2"):
+			bt2.style.backgroundColor = "#16A086";
+			bt2.style.borderColor = "#16A086";
+			bt2.style.boxShadow = "inset 0px -3px #3BD4B6";
+
+			bt1.style.backgroundColor = "#22303D";
+			bt1.style.borderColor = "#FFFFFF";
+			bt1.style.boxShadow = "none";
+			break;
+
+		case "bt1":
+			bt1.style.backgroundColor = "#16A086";
+			bt1.style.borderColor = "#16A086";
+			bt1.style.boxShadow = "inset 0px -3px #3BD4B6";
+
+			bt2.style.backgroundColor = "#22303D";
+			bt2.style.borderColor = "#FFFFFF";
+			bt2.style.boxShadow = "none";
+			break;
+	}				
 };
 				
